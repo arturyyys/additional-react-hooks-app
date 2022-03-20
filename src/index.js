462803fe1589298838e2bbf70afc364d848d7671
arturyyys <arturyyys@gmail.com>
2022-03-20 21:11:47 +0200

    testing react apps

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
-import configureProductsStore from "./hooks-store/products-store";
+import './index.css';
+import App from './App';
 
-configureProductsStore();
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
