14ebefeab2a4d068f3e3c80d1e76fe657f3b0b47
arturyyys <arturyyys@gmail.com>
2022-04-02 21:11:45 +0300

    Diving Into Generics

--- basics.ts
+++ basics.ts
@@ -59,14 +59,14 @@ function print(value: any) {
 
 // Generics
 
-function insetAtBeginning<T>(array: T[], value: T) {
+function insertAtBeginning<T>(array: T[], value: T) {
   const newArray = [value, ...array];
   return newArray;
 }
 
 const demoArray = [1, 2, 3];
 
-const updatedArray = insetAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
 const stringArray = insertAtBeginning(["a", "b", "c"], "d");
 
 // updatedArray[0].split("");
