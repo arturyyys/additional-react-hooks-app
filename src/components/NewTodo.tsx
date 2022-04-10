ec1748707799aec711a07aa607ae8a9472eb4471
arturyyys <arturyyys@gmail.com>
2022-04-10 19:26:20 +0300

    Working with Function Props

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import React from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,6 +13,8 @@ const NewTodo = () => {
       // throw an error
       return;
     }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
